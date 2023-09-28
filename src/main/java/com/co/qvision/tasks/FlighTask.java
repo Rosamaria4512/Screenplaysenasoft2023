9337cd31e3fc6e49ebdef43b960706aa42a91f08
Rosamaria4512 <rmquilindo8@misena.edu.co>
2023-09-28 08:47:30 -0500

    validacion correcta login

--- src/main/java/com/co/qvision/tasks/FlighTask.java
+++ src/main/java/com/co/qvision/tasks/FlighTask.java
@@ -31,29 +31,34 @@ public class FlighTask implements Task {
                 Click.on(FligtPage.BTN_FLIGHT),
                 Click.on(FligtPage.BTN_IMAGE));
         actor.attemptsTo(Click.on(FligtPage.BTN_SEEFLIGHT));try {Thread.sleep(3000);} catch (InterruptedException e) {throw new RuntimeException(e);}
-actor.attemptsTo(Scroll.to(FligtPage.BTN_NETX));
+        actor.attemptsTo(Scroll.to(FligtPage.BTN_NETX));
         actor.attemptsTo(Click.on(FligtPage.BTN_NETX),
                 Click.on(FligtPage.BTN_NETXCONTINUED),
-                Click.on(FligtPage.BTN_NETXCONTINUED1),
-                Enter.theValue(dataFlight.getName()).into(FligtPage.TXT_NAMEA),
+                Click.on(FligtPage.BTN_NETXCONTINUED1));
+        try {Thread.sleep(2000);} catch (InterruptedException e) {throw new RuntimeException(e);}
+        actor.attemptsTo(Enter.theValue(dataFlight.getName()).into(FligtPage.TXT_NAMEA),
                 Enter.theValue(dataFlight.getLastname()).into(FligtPage.TXT_LASTNAME),
-                Click.on(FligtPage.BTN_GENDER),
-                Click.on(FligtPage.BTN_SELECTGENDER),
+                       //Enter.theValue(dataFlight.getLastname()).into(FligtPage.TXXT_LASTNAME),
+                       Click.on(FligtPage.BTN_GENDER),
+                       Click.on(FligtPage.BTN_SELECTGENDER),
+                       Enter.theValue(dataFlight.getDayT()).into(FligtPage.TXT_DAY),
+                       Click.on(FligtPage.BTN_SELECT_MONTH),
+                       Click.on(FligtPage.BTN_MONTH),
+                       Enter.theValue(dataFlight.getYear()).into(FligtPage.TXT_YEARS),
+                       Click.on(FligtPage.BTN_LIST),
+               // Click.on(FligtPage.BTN_GENDER),
+              //  Click.on(FligtPage.BTN_SELECTGENDER),
                 Enter.theValue(dataFlight.getName()).into(FligtPage.TXT_NAMEA),
                 Enter.theValue(dataFlight.getEmail()).into(FligtPage.TXT_EMAILC),
                 Enter.theValue(dataFlight.getPhone()).into(FligtPage.TXT_TEL),
                 Enter.theValue(dataFlight.getName()).into(FligtPage.TXXT_NAME),
-                Enter.theValue(dataFlight.getLastname()).into(FligtPage.TXXT_LASTNAME),
-                Click.on(FligtPage.BTN_GENDER),
-                Click.on(FligtPage.BTN_SELECTGENDER),
-                Click.on(FligtPage.BTN_LIST),
-                //SIGUEE
-                Enter.theValue(dataFlight.getDayT()).into(FligtPage.TXT_DAY),
-                Click.on(FligtPage.BTN_SELECT_MONTH),
-                Click.on(FligtPage.BTN_MONTH),
-                Enter.theValue(dataFlight.getYear()).into(FligtPage.TXT_YEARS),
+
                 Click.on(FligtPage.BTN_NEEXT),
                 Click.on(FligtPage.BTN_NEEXT2),
+
+                //SIGUEE
+
+
                 Enter.theValue(dataFlight.getNametarget()).into(FligtPage.TXT_NAMETARGET),
                 Enter.theValue(dataFlight.getNambertarget()).into(FligtPage.TXT_NUMBER_TARGET),
                 Enter.theValue(dataFlight.getDay()).into(FligtPage.TXT_EXPIREDTARGET),
@@ -61,11 +66,7 @@ actor.attemptsTo(Scroll.to(FligtPage.BTN_NETX));
                 Click.on(FligtPage.BTN_SELECT),
                 Click.on(FligtPage.BTN_FLIGHT),
                 Click.on(FligtPage.BTN_FLIGHT));
-        try {
-            Thread.sleep(15000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
+        try {Thread.sleep(15000);} catch (InterruptedException e) {throw new RuntimeException(e);}
 
 
     }
